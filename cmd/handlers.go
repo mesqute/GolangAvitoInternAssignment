ba0918328fa24e6e698b163619a386b4cdab8072
git <mesuiter@mail.ru>
2022-01-10 21:48:10 +0300

    Структуризация проекта

---
+++ cmd/handlers.go
@@ -0,0 +1,18 @@
+package main
+
+import "net/http"
+
+// Обработчик метода изменения кол-ва средств на балансе
+func updateCashHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// Обработчик метода перевода средств между счетами
+func transferCashHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// Обработчик метода получения текущего баланса
+func getCurrentCashHandler(w http.ResponseWriter, r *http.Request) {
+
+}
--- cmd/main.go
+++ cmd/main.go
@@ -6,32 +6,21 @@ import (
 	"net/http"
 )
 
-// Обработчик метода изменения кол-ва средств на балансе
-func updateCashHandler(w http.ResponseWriter, r *http.Request) {
-
-}
-
-// Обработчик метода перевода средств между счетами
-func transferCashHandler(w http.ResponseWriter, r *http.Request) {
-
-}
-
-// Обработчик метода получения текущего баланса
-func getCurrentCashHandler(w http.ResponseWriter, r *http.Request) {
-
-}
-
 func main() {
 	//переменная,содержащая сетевой адрес дислокации сервера,
 	//получающая значение из флагов командной сроки при запуске приложения
 	//(по умолчанию ":8080")
 	addr := flag.String("addr", ":8080", "сетевой адрес HTTP")
 
+	//считывание флагов из командной строки
+	flag.Parse()
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/updatecash", updateCashHandler)
 	mux.HandleFunc("/transfercash", transferCashHandler)
 	mux.HandleFunc("/getcurrentcash", getCurrentCashHandler)
 
+	log.Printf("Запуск сервера на %s", *addr)
 	err := http.ListenAndServe(*addr, mux)
 	if err != nil {
 		log.Fatal(err)
