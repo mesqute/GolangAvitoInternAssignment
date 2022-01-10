c807ec5f58110fc47573a5be17c8bf859c09edf4
git <mesuiter@mail.ru>
2022-01-10 21:05:18 +0300

    Заложена архитектура обработки методов, а также добавлено получение сетевого адреса дислокации сервера из флагов команды запуска

--- main.go
+++ main.go
@@ -1,5 +1,39 @@
 package main
 
+import (
+	"flag"
+	"log"
+	"net/http"
+)
+
+// Обработчик метода изменения кол-ва средств на балансе
+func updateCashHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// Обработчик метода перевода средств между счетами
+func transferCashHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// Обработчик метода получения текущего баланса
+func getCurrentCashHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
 func main() {
-	
+	//переменная,содержащая сетевой адрес дислокации сервера,
+	//получающая значение из флагов командной сроки при запуске приложения
+	//(по умолчанию ":8080")
+	addr := flag.String("addr", ":8080", "сетевой адрес HTTP")
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/updatecash", updateCashHandler)
+	mux.HandleFunc("/transfercash", transferCashHandler)
+	mux.HandleFunc("/getcurrentcash", getCurrentCashHandler)
+
+	err := http.ListenAndServe(*addr, mux)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
